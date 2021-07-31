54fda39a38d57176b0fb7d9eeb2eb21181f2d83b
find <find@program>
2021-08-01 00:06:29 +0300

    i do have git commit history, i remove no-commit push from program.sh

--- program.sh
+++ program.sh
@@ -54,15 +54,4 @@ j-package(){
   
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am find program"
-  git push -f -u origin main
-}
-
 "$@"
