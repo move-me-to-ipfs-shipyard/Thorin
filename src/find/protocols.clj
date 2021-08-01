06d75d8b1b6b00125ec037b82893cd04cce3c0f4
find <find@program>
2021-08-01 15:03:24 +0300

    to protocols i add expanse.bytes.protocols code

--- src/find/protocols.clj
+++ src/find/protocols.clj
@@ -15,3 +15,20 @@
   (send* [_ data])
   #_Close
   #_IDeref)
+
+(defprotocol IPushbackInputStream
+  (read* [_] [_ length])
+  (unread* [_ char-int])
+  (unread-byte-array* [_ byte-arr] [_ byte-arr offset length]))
+
+(defprotocol IToByteArray
+  (to-byte-array* [_]))
+
+(defprotocol IByteArrayOutputStream
+  (write* [_ char-int])
+  (write-byte-array* [_ byte-arr])
+  (reset* [_])
+  #_IToByteArray)
+
+(defprotocol Closable
+  (close [_]))
