c412f069eb3c63ef10948cfb693ad55c27cba1a4
find <find@program>
2021-09-02 10:36:08 +0300

    i add seed ns

--- src/find/main.clj
+++ src/find/main.clj
@@ -4,7 +4,10 @@
     [clojure.core.async :as a :refer [<! >! <!! >!! chan put! take! go alt! alts! do-alts close! timeout pipe mult tap untap 
                                       pub sub unsub mix admix unmix dropping-buffer sliding-buffer pipeline pipeline-async to-chan! thread]]
     [clojure.string]
-    [clojure.java.io :as io])
+    [clojure.java.io :as io]
+    
+    [find.seed]
+    )
   (:import
     (javax.swing JFrame WindowConstants ImageIcon JTextField JPanel)
   )    
