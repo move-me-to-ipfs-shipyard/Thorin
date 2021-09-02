7df5b443a336f44d2ac544b2880c5dc8ef201878
find <find@program>
2021-09-02 14:10:30 +0300

    in bencode i declare encode decode encode* decode*

--- src/find/bencode.clj
+++ src/find/bencode.clj
@@ -3,7 +3,11 @@
     [clojure.core.async :as a :refer [<! >! <!! >!! chan put! take! go alt! alts! do-alts close! timeout pipe mult tap untap 
                                       pub sub unsub mix admix unmix dropping-buffer sliding-buffer pipeline pipeline-async to-chan! thread]]
     [clojure.string]
-    [clojure.java.io :as io])
+    [clojure.java.io :as io]
+  )
+  (:import
+    (java.io ByteArrayOutputStream ByteArrayInputStream PushbackInputStream)
+  )
 )
 
 (def i-int (int \i))
@@ -11,3 +15,47 @@
 (def d-int (int \d))
 (def l-int (int \d))
 (def colon-int (int \:))
+
+(defmulti encode* 
+  (fn 
+    ([value baos] 
+      (cond
+        (bytes? value) :byte-arr
+        (string? value) :string
+        (map? value) :map
+        (sequential? value) :list
+        (int? value) :int
+      )
+    )
+    ([value baos dispatch-value] dispatch-value)
+  )
+)
+
+(defn encode
+  [data]
+  (let [baos (ByteArrayOutputStream.)]
+    (encode* data baos)
+  )
+)
+
+(defmulti decode*
+  (fn [byte-arr bais] )
+)
+
+(defn decode
+  [byte-arr]
+  (let [bais (-> 
+              (ByteArrayInputStream. byte-arr)
+              (PushbackInputStream.)
+              ) ]
+    (decode* byte-arr bais)
+  )
+)
+
+
+
+(comment
+
+
+
+)
