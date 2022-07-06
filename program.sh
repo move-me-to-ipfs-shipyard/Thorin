b528080bf9d76008078ee58d65870803d7f95f6d
Thorin <Thorin@goat-rider>
2022-07-06 18:34:21 +0300

    i remove commit count - my release is entirely defined by commit hash - we have no age and are evergreen :David-Mitchell but mixed with your current accent - God knows what it could do

--- program.sh
+++ program.sh
@@ -14,24 +14,13 @@ main(){
     -M -m Thorin.main
 }
 
-tag(){
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
-  TAG="$COMMIT_COUNT-$COMMIT_HASH"
-  git tag $TAG $COMMIT_HASH
-  echo $COMMIT_HASH
-  echo $TAG
-}
-
 jar(){
 
   rm -rf out/*.jar out/classes
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
     -X:Genie Genie.core/process \
     :main-ns Thorin.main \
-    :filename "\"out/Thorin-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :filename "\"out/Thorin-$(git rev-parse --short HEAD).jar\"" \
     :paths '["src"]'
 }
 
